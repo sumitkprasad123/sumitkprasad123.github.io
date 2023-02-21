ff727e75b1b1ce0ccfc4c20bfc6ffb567643c597
sumit <sumitkprasad123@gmail.com>
2023-02-21 14:46:16 +0530

    add

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -3,7 +3,165 @@ import "./skill.css"
 
 const Skill = () => {
   return (
-    <div  id="skills">Skill</div>
+    <div  id="skills">
+        <h1>Skills</h1>
+        <div className='skill__container_main'>
+          {/* Frontend Logo */}
+          <div className='skill__container'>
+             <h3>Frontend Development</h3>
+             <div className='skills__logo'>
+                <div className='frontend'>
+
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/html-5--v1.png" alt="html"
+                        />
+                        <p>Html 5</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                        style={{ width: "35px", height: "35px" }}
+                        src="https://img.icons8.com/plasticine/100/null/react.png" alt="react"
+                        />
+                        <p>React</p>        
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/css3.png" alt="css"
+                        />
+                        <p>Css</p>         
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/redux.png" alt="redux"
+                        />
+                        <p>Redux</p>        
+                    </article>  
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/javascript.png" alt="javascript"
+                        />
+                        <p>JavaScript</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/chakra-ui.png" alt="chakra"
+                        />
+                        <p>Chakra UI</p>        
+                    </article>
+                </div>
+             </div>
+          </div>
+          {/* Backend Logo */}
+          <div className='skill__container'>
+             <h3>Backend Development</h3>
+             <div className='skills__logo'>
+                <div className='frontend'>
+
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/html-5--v1.png" alt="html"
+                        />
+                        <p>Html 5</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                        style={{ width: "35px", height: "35px" }}
+                        src="https://img.icons8.com/plasticine/100/null/react.png" alt="react"
+                        />
+                        <p>React</p>        
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/css3.png" alt="css"
+                        />
+                        <p>Css</p>         
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/redux.png" alt="redux"
+                        />
+                        <p>Redux</p>        
+                    </article>  
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/javascript.png" alt="javascript"
+                        />
+                        <p>JavaScript</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/chakra-ui.png" alt="chakra"
+                        />
+                        <p>Chakra UI</p>        
+                    </article>
+                </div>
+             </div>
+          </div>
+          {/* Tools Logo */}
+          <div className='skill__container'>
+             <h3>Tools</h3>
+             <div className='skills__logo'>
+                <div className='frontend'>
+
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/html-5--v1.png" alt="html"
+                        />
+                        <p>Html 5</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                        style={{ width: "35px", height: "35px" }}
+                        src="https://img.icons8.com/plasticine/100/null/react.png" alt="react"
+                        />
+                        <p>React</p>        
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/css3.png" alt="css"
+                        />
+                        <p>Css</p>         
+                    </article> 
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/redux.png" alt="redux"
+                        />
+                        <p>Redux</p>        
+                    </article>  
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/fluency/48/null/javascript.png" alt="javascript"
+                        />
+                        <p>JavaScript</p>        
+                    </article>
+                    <article className='skills__details'>
+                        <img
+                          style={{ width: "35px", height: "35px" }}
+                          src="https://img.icons8.com/color/48/null/chakra-ui.png" alt="chakra"
+                        />
+                        <p>Chakra UI</p>        
+                    </article>
+                </div>
+             </div>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
